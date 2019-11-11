0bc8b70768a9e3bb44bd21d8cb3718eb4018be89
damian-dz <damiandzm@gmail.com>
2019-11-11 16:36:31 +0100

    backup before branching

---
+++ SvgCodeGen/Templates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SvgCodeGen
+{
+    public static class Templates
+    {
+        public static SvgGroup Grid(double x, double y, double width, double height, int cellsX, int cellsY)
+        {
+            var group = new SvgGroup();
+            var rect = new SvgRectangle(x, y, width, height)
+            {
+                StrokeWidth = 3
+            };
+            double stepX = width / cellsX;
+            double stepY = height / cellsY;
+            group.AddElement(rect);
+            for (double i = x + stepX; i < x + width; i += stepX) {
+                var line = new SvgLine(i, y, i, y + height);
+                group.AddElement(line);
+            }
+            for (double i = y + stepY; i < y + height; i += stepY) {
+                var line = new SvgLine(x, i, x + width, i);
+                group.AddElement(line);
+            }
+            return group;
+        }
+    }
+}
